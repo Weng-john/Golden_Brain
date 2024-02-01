49a2f41987ca2af900d4e13df43fd72d1a453d9d
Weng-john <59682713+Weng-john@users.noreply.github.com>
2024-02-01 22:47:56 +0800

    architecture done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,41 @@
-var formUrl=[
-    "https://forms.gle/FhQb41Bn8yHHpyaW9",
-    "https://forms.gle/3HTu36nTc7VcdAow7",
-    "https://forms.gle/ttSnEUJGUhPnhEAR9",
-    "https://forms.gle/3cNA9ydkbh6jaZ6Q7",
-    "https://forms.gle/q7JRAFfcDzvm2p8f8",
-    "https://forms.gle/eet3YEvXTtxBjDZP7",
-    "https://forms.gle/bHfriWwBba7pctUn8",
-    "https://forms.gle/PySHaiebGQfapx4i7",
-    "https://forms.gle/9EtpJBcggX4a53q38",
-    "https://forms.gle/MMW5KUjJy46WtUZe6",
-    "https://forms.gle/YckbXWVUzyTyzZHj9"
-];
+const ROWS = 10;
+const COLS = 10;
+const minefield = document.getElementById("minefield");
+const tbody = document.createElement("tbody");
 
-document.querySelector(".confirm").addEventListener("click",()=>{
-    let sel= document.getElementById("race").value;
-    if(sel%1===0){
-        document.getElementById("loadingArea").classList.add("loadingArea");
-        window.location.href= formUrl[sel-1];
-        setTimeout(()=>{
-            document.getElementById("loadingArea").classList.remove("loadingArea");
-        }, 5000); 
-    }
-});
+// 添加列编号
+let colRow = document.createElement("tr");
+colRow.innerHTML = "<th class='col-index'></th>";
+for (let i = 1; i <= COLS; i++) {
+  let colNumber = document.createElement("th");
+  colNumber.textContent = i;
+  colNumber.classList.add("col-index");
+  colRow.appendChild(colNumber);
+}
+tbody.appendChild(colRow);
+
+// 添加行和行编号
+for (let i = 1; i <= ROWS; i++) {
+  let row = document.createElement("tr");
+  let rowNumber = document.createElement("th");
+  rowNumber.textContent = i;
+  rowNumber.classList.add("row-index");
+  row.appendChild(rowNumber);
+  for (let j = 1; j <= COLS; j++) {
+    let cell = document.createElement("td");
+    cell.dataset.row= i;
+    cell.dataset.col= j;
+    cell.addEventListener("click", handleCellClick);
+    row.appendChild(cell);
+  }
+  tbody.appendChild(row);
+}
+
+function handleCellClick(event) {
+    const row = parseInt(event.target.dataset.row);
+    const col = parseInt(event.target.dataset.col);
+    alert(`Clicked on cell (${row}, ${col})`);
+    // 在这里可以添加逻辑来处理点击事件，比如检查是否触发了地雷等等。
+}
+
+minefield.appendChild(tbody);
